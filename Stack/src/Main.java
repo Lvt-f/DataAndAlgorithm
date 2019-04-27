4dc18baed821aa51fefe1f8a9267dea4d45fd61f
Vincent <vincent@Vincent.local>
2019-04-27 20:48:23 +0800

    添加部分内容

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,16 +1,17 @@
 import java.util.Random;
+//其实这个时间比较很复杂，因为LinkedListStack中包含更多的new操作
 public class Main {
     public static void main(String[] args)
     {
-        int opCount = 100000;
+        int opCount = 10000000;
 
         ArrayStack<Integer>arrayStack = new ArrayStack<>();
         double time1 = testStack(arrayStack,opCount);
-        System.out.println("arrayStack time:" + time1 +"S");
+        System.out.println("ArrayStack time:" + time1 +"S");
 
         LinkedListStack<Integer>linkedListStack = new LinkedListStack<>();
         double time2 = testStack(linkedListStack,opCount);
-        System.out.println("linkedListStack time:" + time2 +"S");
+        System.out.println("LinkedListStack time:" + time2 +"S");
     }
     //测试使用stack运行opCount个push和pop操作所需要的时间，单位，秒
     private static double testStack(Stack<Integer> stack,int opCount)
